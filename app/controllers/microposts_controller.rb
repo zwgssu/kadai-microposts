2e07d347f148b330dc468b82515330a96fa811df
h-150 <tokioka.moher@gmail.com>
2021-06-25 06:12:18 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,6 +8,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "メッセージを投稿しました"
       redirect_to root_url
     else
+      @pagy, @microposts = pagy(current_user.feed_microposts.order(id: :desc))
       flash[:danger] = "メッセージの投稿に失敗しました"
       render "toppages/index"
     end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,7 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
     @micropost = current_user.microposts.build #form_with用
-    @pagy, @microposts = pagy(current_user.microposts.order(id: :desc))
+    @pagy, @microposts = pagy(current_user.feed_microposts.order(id: :desc))
     end
   end
 end
